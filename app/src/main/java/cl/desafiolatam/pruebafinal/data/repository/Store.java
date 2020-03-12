54aa7d75f74e1612ade5da182f5abb8252fdf5cd
Nicolas <nifradich@hotmail.com>
2020-03-12 10:17:43 -0300

    avance hasta punto 5 hito 1

---
+++ app/src/main/java/cl/desafiolatam/pruebafinal/data/repository/Store.java
@@ -0,0 +1,5 @@
+package cl.desafiolatam.pruebafinal.data.repository;
+
+public class Store {
+
+}
---
+++ app/src/main/java/cl/desafiolatam/pruebafinal/domain/interactor/UseCaseExtend.java
@@ -0,0 +1,10 @@
+package cl.desafiolatam.pruebafinal.domain.interactor;
+
+import io.reactivex.Observable;
+
+public class UseCaseExtend  extends UseCase{
+    @Override
+    Observable buildUseCaseObservable(Object o) {
+        return null;
+    }
+}
